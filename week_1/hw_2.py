4b54b121ca64e578e3e429400a356931f6e835ff
Alexander Kuznetsov <seelk73@gmail.com>
2018-03-07 03:57:37 +0300

    add homework

--- week_1/hw_2.py
+++ week_1/hw_2.py
@@ -1,7 +1,5 @@
-import sys
+import  sys
 
-coef =[int(sys.argv[i]) for i in range(1, 4, 1)]
-D = (coef[1] ** 2 - 4 * coef[0] * coef[2])**0.5
-
-print(int((-coef[1] - D) / (2 * coef[0])))
-print(int((-coef[1] + D) / (2 * coef[0])))
+count = int(sys.argv[1])
+for i in range(1, count + 1, 1):
+    print(" " * (count - i) + "#" * (i))
